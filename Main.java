73aa4e82fc1e774b0d41adebe42e43a55bc151b9
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-02 14:57:07 +0000

    Making a user enter a positive integer until they enter a negative integer for it to be done

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * a program that will get a user to enter a positive integer and finish when a person enters a negative integer
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+
+    // Ask the user for a number
+    System.out.println("Please enter a positive integer. Enter a negative integer to quit");
+    int number = input.nextInt();
+
+    // Make sure it is a positive integer
+    while (number >= 0){
+    // Ask for a new number
+    System.out.println("Please enter a positive integer. Enter a negative integer to quit");
+    number = input.nextInt();
+    } 
+    System.out.println("All done!");
+
+
     
   }
 }
